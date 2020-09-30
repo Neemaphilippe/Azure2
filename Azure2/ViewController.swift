24014e290d3376923ff1721db737ec077b73b019
Neema Philippe <neemaphilippe@pursuit.org>
2020-09-30 12:35:46 -0400

    first commit

---
+++ Azure2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Azure2
+//
+//  Created by Neema on 9/30/20.
+//  Copyright © 2020 Neema Philippe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Azure2Tests/Azure2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Azure2Tests.swift
+//  Azure2Tests
+//
+//  Created by Neema on 9/30/20.
+//  Copyright © 2020 Neema Philippe. All rights reserved.
+//
+
+import XCTest
+@testable import Azure2
+
+class Azure2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
